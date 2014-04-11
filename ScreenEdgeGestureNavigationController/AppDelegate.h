99efa9f2cc882aeeba146d6f29aad69cd9d7dd2c
Rob Ryan <robert.ryan@mindspring.com>
2014-04-11 11:46:27 -0400

    Initial Commit

---
+++ ScreenEdgeGestureNavigationController/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ScreenEdgeGestureNavigationController
+//
+//  Created by Robert Ryan on 4/11/14.
+//  Copyright (c) 2014 Robert Ryan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ScreenEdgeGestureNavigationControllerTests/ScreenEdgeGestureNavigationControllerTests.m
@@ -0,0 +1,34 @@
+//
+//  ScreenEdgeGestureNavigationControllerTests.m
+//  ScreenEdgeGestureNavigationControllerTests
+//
+//  Created by Robert Ryan on 4/11/14.
+//  Copyright (c) 2014 Robert Ryan. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ScreenEdgeGestureNavigationControllerTests : XCTestCase
+
+@end
+
+@implementation ScreenEdgeGestureNavigationControllerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
